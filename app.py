a70aa60b7f3d7f6364fd81b4b8edaf47ad9fe626
aayushxrj <aayush.ranjan0904@gmail.com>
2024-04-13 10:48:42 +0530

    [Progress]Chatbot working

--- app.py
+++ app.py
@@ -13,7 +13,15 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 import chainlit as cl 
 from chainlit.types import AskFileResponse
 
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from typing import Dict
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.messages import HumanMessage
+
+
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
+llm = ChatAnthropic(temperature=0, model_name="claude-3-opus-20240229")
 
 welcome_message = """Welcome to the Pluto.ai ! To get started:
 1. Upload a PDF or text file
@@ -61,15 +69,14 @@ def store_embeddings(chunks):
     return vectordb
 
 # Retrieve data from the query
-# def simple_retrieval(vectordb,message: cl.Message):
-#     query = message.content   
+def simple_retrieval(vectordb, message: cl.Message):
+    query = message.content
 
-#     results = vectordb.similarity_search(query, n_results=5)
-#     retrieved_documents = results['documents'][0]
-#     print(results)
-#     print("\n\n")
-#     print(f"Retrieved documents: {retrieved_documents}")
-#     return results # debugging return 
+    results = vectordb.similarity_search(query, k=5)
+    retrieved_documents = [result.page_content for result in results]
+
+    return retrieved_documents
+
 
 @cl.step
 def tool():
@@ -104,7 +111,8 @@ async def start():
 
     # Retrieve data from the query
     # retrived_documents = simple_retrieval(vectordb, cl.Message(content="What is the regression?"))
-
+    # print(retrived_documents)
+    
     message_history = ChatMessageHistory()
 
     memory = ConversationBufferMemory(
@@ -113,28 +121,57 @@ async def start():
         chat_memory=message_history,
         return_messages=True,
     )
-    chain = ConversationalRetrievalChain.from_llm(
-        ChatAnthropic(temperature=0, model_name="claude-3-opus-20240229"),
-        chain_type="stuff",
-        retriever=vectordb.as_retriever(), 
-        memory=memory,
-        return_source_documents=True,
+
+    SYSTEM_TEMPLATE = """
+    Answer the user's questions based on the below context. 
+    If the context doesn't contain any relevant information to the question, don't make something up and just say "I don't know":
+
+    <context>
+    {context}
+    </context>
+    """
+
+    question_answering_prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                SYSTEM_TEMPLATE,
+            ),
+            MessagesPlaceholder(variable_name="messages"),
+        ]
+    )
+
+    document_chain = create_stuff_documents_chain(llm, question_answering_prompt)
+
+    
+    def parse_retriever_input(params: Dict):
+        return params["messages"][-1].content
+
+
+    chain = RunnablePassthrough.assign(
+        context=parse_retriever_input | vectordb.as_retriever(k=5),
+    ).assign(
+        answer=document_chain,
     )
 
     msg.content = f"`{file.name}` processed. You can now ask questions!"
     await msg.update()
 
-    
-
+    cl.user_session.set("chain", chain)
 
 
 @cl.on_message
 async def main(message: cl.Message):
-
-    # chain = cl.user_session.get("chain")  # type: ConversationalRetrievalChain
-    # cb = cl.AsyncLangchainCallbackHandler()
-    # response = await chain.acall(message.content, callbacks=[cb])
-    # print(response)
-
+    chain = cl.user_session.get("chain")
+    
+    # response = await chain.acall(message.content, 
+    #                                  callbacks=[
+    #                                      cl.AsyncLangchainCallbackHandler()])
+    response = chain.invoke(
+        {"messages": [
+            HumanMessage(content=message.content)
+        ],
+    }
+    )
     tool()
-    await cl.Message(content="Response not working yet").send()
+    await cl.Message(response["answer"]).send()
