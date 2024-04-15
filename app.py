a85222580ae32e163a65df087a28198af85ae47d
aayushxrj <aayush.ranjan0904@gmail.com>
2024-04-15 22:11:35 +0530

    explored load_qa_chain approach

--- app.py
+++ app.py
@@ -20,6 +20,10 @@ from typing import Dict
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.messages import HumanMessage
 
+# For Approach 3
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+
 
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 llm = ChatAnthropic(temperature=0, model_name="claude-3-opus-20240229")
@@ -69,13 +73,13 @@ def store_embeddings(chunks):
     return vectordb
 
 # Retrieve data from the query
-def simple_retrieval(vectordb, message: cl.Message):
-    query = message.content
+# def simple_retrieval(vectordb, message: cl.Message):
+#     query = message.content
 
-    results = vectordb.similarity_search(query, k=5)
-    retrieved_documents = [result.page_content for result in results]
+#     results = vectordb.similarity_search(query, k=5)
+#     retrieved_documents = [result.page_content for result in results]
 
-    return retrieved_documents
+#     return retrieved_documents
 
 @cl.step
 def ChainOfThought():
@@ -105,19 +109,17 @@ async def start():
     # Process the file and split into chunks
     chunks = split_into_chunks(file)
 
+    msg.content=f"Creating chunks for `{file.name}`..."
+    await msg.update()
+
     # Store the data in form of embeddings
     vectordb = store_embeddings(chunks)
 
+    msg.content = f"Creating embeddings for `{file.name}`. . ."
+    await msg.update()
 
-    message_history = ChatMessageHistory()
-
-    memory = ConversationBufferMemory(
-        memory_key="chat_history",
-        output_key="answer",
-        chat_memory=message_history,
-        return_messages=True,
-    )
     # Approach 1 (low level approach)
+
     # SYSTEM_TEMPLATE = """
     # Answer the user's questions based on the below context. 
     # If the context doesn't contain any relevant information to the question, don't make something up and just say "I don't know":
@@ -153,17 +155,55 @@ async def start():
 
 
     # Approach 2 (high level approach)
+
+    message_history = ChatMessageHistory()
+
+    memory = ConversationBufferMemory(
+        memory_key="chat_history",
+        input_key="question",
+        output_key="answer",
+        chat_memory=message_history,
+        return_messages=True,
+    )
+
     chain = ConversationalRetrievalChain.from_llm(
         llm,
         chain_type="stuff",
-        retriever=vectordb.as_retriever(),
+        retriever=vectordb.as_retriever(search_type="similarity", search_kwargs={"k":5}),
         memory=memory,
         return_source_documents=True,
     )
-    if chain.memory is not None:
-        print("The conversational retrieval chain has memory.")
-    else:
-        print("The conversational retrieval chain does not have memory.")
+
+    # debugging
+    # if chain.memory is not None:
+    #     print("The conversational retrieval chain has memory.")
+    # else:
+    #     print("The conversational retrieval chain does not have memory.")
+
+    # Approach 3 (using load_qa_chain)
+
+    # template="""  Answer the user's questions based on the below context and the previous chat history. 
+    # If the context doesn't contain any relevant information to the question, don't make something up and just say "I don't know":
+
+    # <context>
+    # {context}
+    # </context>
+
+    # <question>
+    # {question}
+    # </question>
+
+    # <chat_history>
+    # {chat_history}
+    # </chat_history>
+
+    # ANSWER :
+    # """
+    # memory = ConversationBufferMemory(memory_key="chat_history", input_key="question",max_len=50,return_messages=True)
+    # prompt = PromptTemplate(input_variables=["chat_history", "context", "question"], template=template)
+ 
+    # chain = load_qa_chain(llm, chain_type="stuff", memory=memory, prompt=prompt)
+    # cl.user_session.set("vectordb", vectordb)
 
 
     msg.content = f"`{file.name}` processed. You can now ask questions!"
@@ -182,8 +222,9 @@ async def main(message: cl.Message):
     #     ],
     # })
 
-    #Approach 2
+    # Approach 2
     response = await chain.acall(message.content, callbacks=[cl.AsyncLangchainCallbackHandler()])
+    print(response)                  # debugging
     answer = response["answer"]
     source_documents = response["source_documents"]
     text_elements = []
@@ -206,6 +247,47 @@ async def main(message: cl.Message):
         else:
             answer += "\n\nNo sources found"
 
-    ChainOfThought()
+    # ChainOfThought()
 
     await cl.Message(content=answer, elements=text_elements).send()
+
+
+
+    # Approach 3    (Sources link now working and old file cache issue)
+
+    # vectordb = cl.user_session.get("vectordb")
+    # docs = vectordb.similarity_search(query=message.content,k=5)
+
+    # # what is the use of context when input_documents is acting as the context in load_qa_chain
+
+    # chain_input={
+    # "input_documents": docs,
+    # "context":"This is contextless", 
+    # "question":message.content
+    # }
+
+    # response = chain(chain_input, callbacks=[cl.AsyncLangchainCallbackHandler()])   # callbacks=[cl.AsyncLangchainCallbackHandler()] missing
+    # print(response)                  # debugging
+
+    # answer = response["output_text"]
+    # # print source documents
+    # source_documents = response["input_documents"]
+    # text_elements = []
+    # unique_pages = set()
+    # if source_documents:
+    #     for source_idx, source_doc in enumerate(source_documents):
+    #         source_name = f"source_{source_idx}"
+    #         page_number = source_doc.metadata['page']
+    #         page = f"Page {page_number}"
+    #         text_element_content = source_doc.page_content
+    #         if page not in unique_pages:
+    #             unique_pages.add(page)
+    #             text_elements.append(cl.Text(content=text_element_content, name=page))
+    #     source_names = [text_el.name for text_el in text_elements]
+    #     if source_names:
+    #         answer += f"\n\nSources: {', '.join(source_names)}"
+    #     else:
+    #         answer += "\n\nNo sources found"
+
+    # await cl.Message(content=answer).send()
+
